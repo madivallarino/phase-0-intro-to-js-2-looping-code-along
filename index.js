504c732c8d1391b0672e7397aa1f91f21ac0ac99
Madison Vallarino <madivallarino@gmail.com>
2021-07-31 12:40:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(array, string){
+    const message = []
+    for (let i = 0; i < array.length; i++){
+        message.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return message;
+}
+
+writeCards(["Madi", "Mel", "Rae"], "birthday");
+
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+}
